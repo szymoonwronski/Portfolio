fd724af34c4640d29ee302ee718c902c6cfc9077
Szymon Wronski <szymoon.wronski@gmail.com>
2023-05-03 14:03:16 +0200

    typo fix

--- src/components/ProjectsSegment.jsx
+++ src/components/ProjectsSegment.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import icon1 from "./../assets/icon1.png"
-import icon2 from "./../assets/ucon2.png"
+import icon2 from "./../assets/icon2.png"
 import icon3 from "./../assets/icon3.png"
 
 const ProjectsSegment = () => {
